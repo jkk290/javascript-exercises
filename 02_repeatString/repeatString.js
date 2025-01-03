0efea1af11f8d1ccd65d4d6b33250ad8656e1536
jkk290 <158115736+jkk290@users.noreply.github.com>
2025-01-02 20:33:01 -1000

    Complete exercise 2
    Completed 02_REPEATSTRING exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let i = 0;
+    let repeatedString = "";
+    if (num >= 0) {
+        while (i < num) {
+        repeatedString += string;
+            i++;
+        }
+    } else {
+        return("ERROR");
+    }
+    return(repeatedString);
 };
 
 // Do not edit below this line
