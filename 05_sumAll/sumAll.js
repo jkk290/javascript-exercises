bc4284a24dbe36f0d8b87c7004a511195546909b
jkk290 <158115736+jkk290@users.noreply.github.com>
2025-01-12 14:18:42 -1000

    Complete exercise 5
    Completed exercise 5, sumAll.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if ((num1 < 0) || (num2 < 0)) {
+        return("ERROR");
+    } else if ((!Number.isInteger(num1)) || (!Number.isInteger(num2))) {
+        return("ERROR")
+    } else {
+        let minNum = Math.min(num1, num2);
+        let maxNum = Math.max(num1, num2);
+        let runningTotal = 0;
+        for (let i = minNum; i <= maxNum; i++ ) {
+            runningTotal += i;
+        };
+        return(runningTotal);
+    };
+    
 };
 
 // Do not edit below this line
