5409c34c85090283d728cbb624566997509c6991
jkk290 <158115736+jkk290@users.noreply.github.com>
2025-02-01 22:36:42 -1000

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(addNum1, addNum2) {
+  let addedNums = addNum1 + addNum2;
+  return addedNums; 
 };
 
-const subtract = function() {
-	
+const subtract = function(subtractedNum1, subtractedNum2) {
+	let subtractedNums = subtractedNum1 - subtractedNum2;
+  return subtractedNums;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let arrSum = 0;
+  arr.forEach(arrNum => {
+    arrSum += arrNum;
+  });
+  return arrSum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let arrayMultiply = 1;
+  arr.forEach(arrNum => {
+    arrayMultiply *= arrNum;
+  });
+  return arrayMultiply;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+	let result = 1;
+  for (let i = 1; i <= num1; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
