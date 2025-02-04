d0a2ddbfe1280bb725d75a7987f6abde47e1c1e4
jkk290 <158115736+jkk290@users.noreply.github.com>
2025-02-03 18:13:48 -1000

    Create initial palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (string) {
+   let reversedArray = Array.from(string).reverse(); 
+   let reversedString = reversedArray.join('');
 
+   if (string.toLowerCase() === reversedString.toLowerCase()) {
+        return true;
+   } return false;
 };
 
 // Do not edit below this line
