dd06a64096e1976a98a1338a5fbf5a09c2f70d33
jkk290 <158115736+jkk290@users.noreply.github.com>
2025-02-04 20:51:31 -1000

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+  let fNumber = parseInt(number);
 
+  if (fNumber >= 0) {
+    let fArray = [0,1];
+    let num1 = 0;
+    let num2 = 1;
+    let tempNum = 0;
+    let i = 0;
+
+    while (i < fNumber) {
+      tempNum = num1 + num2;
+      fArray.push(tempNum);
+      num1 = num2;
+      num2 = tempNum;
+      i++;
+    } return fArray.at(fNumber);
+
+  } return "OOPS"
 };
 
 // Do not edit below this line
