39dc98a681aecb393c7524409f254f2ba8838eff
jkk290 <158115736+jkk290@users.noreply.github.com>
2025-02-05 13:02:20 -1000

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function (peopleArray) {
+  const oldest = peopleArray.sort(function (a, b) {
+    const currentYear = 2025;
 
+    if (a.yearOfDeath == undefined) {
+      const lastPerson = currentYear - a.yearOfBirth;
+      const nextPerson = b.yearOfDeath - b.yearOfBirth;
+      return lastPerson > nextPerson ? -1 : 1;
+    } else if (b.yearOfDeath == undefined) {
+      const lastPerson = a.yearOfDeath - a.yearOfBirth;
+      const nextPerson = currentYear - b.yearOfBirth;
+      return lastPerson > nextPerson ? -1 : 1;
+    } else {
+      const lastPerson = a.yearOfDeath - a.yearOfBirth;
+      const nextPerson = b.yearOfDeath - b.yearOfBirth;
+      return lastPerson > nextPerson ? -1 : 1;
+    }
+  });
+  return oldest.at(0);
 };
 
 // Do not edit below this line
